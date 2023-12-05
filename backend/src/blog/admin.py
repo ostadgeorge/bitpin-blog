4d5bdea33e609627feba3939705f18cfc55472d3
Mohammad Ali Heydari <mohammad.heydari@divar.ir>
2023-12-05 20:22:06 +0330

    Add models to admin panel

--- backend/src/blog/admin.py
+++ backend/src/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from blog.models import BlogEntry, RatingHistory, UserProfile
 
-# Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(BlogEntry)
+admin.site.register(RatingHistory)
